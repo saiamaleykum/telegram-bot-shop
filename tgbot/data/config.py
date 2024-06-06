63c934799251ee10c8be7855764b2789865753f2
egorik <yegorkasvintsov575@gmail.com>
2024-06-06 03:46:07 +0300

    first commit

---
+++ tgbot/data/config.py
@@ -0,0 +1,48 @@
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+BOT_TOKEN: str = env.str("BOT_TOKEN")
+
+LOGGING_LEVEL: int = env.int("LOGGING_LEVEL", 10)
+
+POSTGRES_HOST: str = env.str("POSTGRES_HOST")
+POSTGRES_PORT: str = env.str("POSTGRES_PORT")
+POSTGRES_USER: str = env.str("POSTGRES_USER")
+POSTGRES_PASSWORD: str = env.str("POSTGRES_PASSWORD")
+POSTGRES_DB: str = env.str("POSTGRES_DB")
+
+DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+ACCOUNT_ID: str = env.str("ACCOUNT_ID")
+SECRET_KEY: str = env.str("SECRET_KEY")
+
+CHAT_NAME: str = env.str("CHAT_NAME")
+CHANNEL_NAME: str = env.str("CHANNEL_NAME")
+
+USE_CACHE: bool = env.bool("USE_CACHE", False)
+
+if USE_CACHE:
+    CACHE_HOST: str = env.str("CACHE_HOST")
+    CACHE_PORT: int = env.int("CACHE_PORT")
+    CACHE_PASSWORD: str = env.str("CACHE_PASSWORD")
+
+USE_WEBHOOK: bool = env.bool("USE_WEBHOOK", False)
+
+if USE_WEBHOOK:
+    MAIN_WEBHOOK_ADDRESS: str = env.str("MAIN_WEBHOOK_ADDRESS")
+    MAIN_WEBHOOK_SECRET_TOKEN: str = env.str("MAIN_WEBHOOK_SECRET_TOKEN")
+
+    MAIN_WEBHOOK_LISTENING_HOST: str = env.str("MAIN_WEBHOOK_LISTENING_HOST")
+    MAIN_WEBHOOK_LISTENING_PORT: int = env.int("MAIN_WEBHOOK_LISTENING_PORT")
+
+    MAX_UPDATES_IN_QUEUE: int = env.int("MAX_UPDATES_IN_QUEUE", 100)
+
+USE_CUSTOM_API_SERVER: bool = env.bool("USE_CUSTOM_API_SERVER", False)
+
+if USE_CUSTOM_API_SERVER:
+    CUSTOM_API_SERVER_IS_LOCAL: bool = env.bool("CUSTOM_API_SERVER_IS_LOCAL")
+    CUSTOM_API_SERVER_BASE: str = env.str("CUSTOM_API_SERVER_BASE")
+    CUSTOM_API_SERVER_FILE: str = env.str("CUSTOM_API_SERVER_FILE")
